99c646c2a3be2836f8f09aee265bd3da10878901
DX <dradiance9@163.com>
2021-04-26 23:03:18 +0800

    version v0.2 by dx

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,8 +2,9 @@
  * @author Dx
  * @create 2021/4/26 - 22:27
  */
-public class HellowGit {
+public class HelloGit {
     public static void main(String[] args) {
+        System.out.println("Hello Git");
         System.out.println("Hello Git");
     }
 }
