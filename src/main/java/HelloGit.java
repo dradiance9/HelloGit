6c80840abc7bbfcc4b77e765ce1cb3b86977a11f
DX <dradiance9@163.com>
2021-04-26 23:06:20 +0800

    version v0.3 by test

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -6,5 +6,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        System.out.println("Hello Git  test");
+        System.out.println("Hello Git  test");
     }
 }
